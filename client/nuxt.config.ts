cfb7676f49bc686a6503f664394c677c244045cb
mnenie <1alexpeshkov@gmail.com>
2024-05-13 11:31:09 +0300

    add: base nuxt structure + shadcn

---
+++ client/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"], 
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui'
+  }
+})
