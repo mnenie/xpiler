8af3ce6fe3a864c9b5911ba31233c36f74208d08
mnenie <1alexpeshkov@gmail.com>
2024-05-13 17:43:27 +0300

    add: editor

---
+++ client/components/editor/Monaco.client.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { VueMonacoEditor, useMonaco } from "@guolao/vue-monaco-editor";
+import type * as monaco from "monaco-editor";
+
+const { monacoRef } = useMonaco();
+
+const editorRef = ref<monaco.editor.IEditor>();
+const content = ref(`const a: number = 10;
+console.log(a);`);
+
+onMounted(() => {
+  const monaco = monacoRef.value;
+  if (!monaco) return;
+  const uri = monaco.Uri.parse("post.ts");
+  monaco.editor.createModel(content.value, "typescript", uri);
+});
+
+onUnmounted(() => {
+  editorRef.value?.dispose();
+});
+</script>
+
+<template>
+  <vue-monaco-editor
+    v-model:value="content"
+    :default-language="'typescript'"
+  />
+</template>
