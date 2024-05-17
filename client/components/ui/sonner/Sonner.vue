262c977480a016567ebb5b6b606f2410885080c7
mnenie <1alexpeshkov@gmail.com>
2024-05-18 02:00:22 +0300

    fix: sonner

--- client/components/ui/sonner/Sonner.vue
+++ client/components/ui/sonner/Sonner.vue
@@ -10,8 +10,8 @@ const props = defineProps<ToasterProps>()
     v-bind="props"
     :toast-options="{
       classes: {
-        toast: 'group toast group-[.toaster]:bg-white group-[.toaster]:text-zinc-950 group-[.toaster]:border-zinc-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-zinc-800 dark:group-[.toaster]:text-zinc-50 dark:group-[.toaster]:border-zinc-800 md:group-[.toaster]:text-[12px]',
-        description: 'group-[.toast]:text-gray-500 dark:group-[.toast]:text-gray-400',
+        toast: 'group toast group-[.toaster]:bg-white group-[.toaster]:text-zinc-950 group-[.toaster]:border-zinc-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-zinc-800 dark:group-[.toaster]:text-zinc-50 dark:group-[.toaster]:border-zinc-800 md:group-[.toaster]:text-[12px] 2xl:group-[.toaster]:text-sm',
+        description: 'group-[.toast]:text-zinc-500 dark:group-[.toast]:text-zinc-400',
         actionButton:
           'group-[.toast]:bg-gray-900 group-[.toast]:text-gray-50 dark:group-[.toast]:bg-gray-50 dark:group-[.toast]:text-gray-900',
         cancelButton:
