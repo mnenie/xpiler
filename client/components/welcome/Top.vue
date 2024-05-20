88b561d40fa89b002c30d605030bda0c19e4c299
mnenie <121057011+mneniee@users.noreply.github.com>
2024-05-21 01:59:04 +0300

    done: adaptive for mobiles /welcome page

--- client/components/welcome/Top.vue
+++ client/components/welcome/Top.vue
@@ -7,8 +7,10 @@ import { Braces } from "lucide-vue-next";
   <header
     class="sticky top-0 z-50 w-full border-b border-[#e4e4e766]/40 bg-[#fff9]/95 backdrop-blur supports-[backdrop-filter]:bg-[#fff9]/60 dark:bg-transparent dark:border-zinc-700"
   >
-    <div class="flex h-12 items-center justify-between px-10 pl-20">
-      <div class="flex items-center space-x-2 cursor-pointer dark:text-zinc-100 text-zinc-900">
+    <div class="flex h-12 items-center justify-between px-10 pl-20 header">
+      <div
+        class="flex items-center space-x-2 cursor-pointer dark:text-zinc-100 text-zinc-900"
+      >
         <Braces :size="22" />
         <span class="text-[22px] font-bold dark:text-zinc-100">Xpiler</span>
       </div>
@@ -34,3 +36,23 @@ import { Braces } from "lucide-vue-next";
     </div>
   </header>
 </template>
+
+<style scoped>
+@media screen and (max-width: 1200px) {
+  .header {
+    padding: 0 40px;
+  }
+}
+
+@media screen and (max-width: 586px) {
+  .header {
+    padding: 0 20px;
+  }
+  .header span{
+    font-size: 18px;
+  }
+  .header button{
+    font-size: 12px;
+  }
+}
+</style>
--- client/layouts/welcome.vue
+++ client/layouts/welcome.vue
@@ -1,6 +1,6 @@
 <template>
   <ClientOnly>
-    <div class="relative flex min-h-screen flex-col dark:bg-[#1a1a1a]">
+    <div class="relative flex h-dvh flex-col dark:bg-[#1a1a1a]">
       <WelcomeTop />
       <slot class="flex-1" />
       <WelcomeBottom />
